de9610b6cc27c89c1477f2f5149e11c90175347b
Suiunbek02 <abasbekovsuiunbek@gmail.com>
2024-02-21 00:10:03 +0600

    ok

---
+++ app/src/main/java/com/example/noteappcompose/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.noteappcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val DarPrimary = Color(0xFF36A2EB)
+val DarkSecondery = Color(0xFF5DD38A)
+val DarkTertiary = Color(0xFFEFB8C8)
+val DarkBackground = Color(0xFF1A1A2C)
+val DarkSurface = Color(0xFF2D2D4D)
+val DarkOnSurface = Color(0xFFEAEAEA)
+
